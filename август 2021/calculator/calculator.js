8abf3f97d1ed61ecccdacd22fbb4c843a5a2801d
AleksGordeev <61626344+AleksGordeev@users.noreply.github.com>
2021-09-01 17:58:16 +0700

    a
    Final Version

--- август 2021/calculator/calculator.js
+++ август 2021/calculator/calculator.js
@@ -6,14 +6,13 @@ var numberMinusButton = document.getElementById('minusBtn');
 var numberMultiplyButton = document.getElementById('multiplyBtn');
 var numberDivideButton = document.getElementById('divideBtn');
 
-
 var getNumberFromInput = function (inputParam) {
     var valueAsString = inputParam.value;
     var valueAsNumber = new Number(valueAsString);
     return valueAsNumber;
 }
 
-    var getNumbers = function () {
+var getNumbers = function () {
     var numberValue1 = getNumberFromInput(number1Input);
     var numberValue2 = getNumberFromInput(number2Input);
     var result = {
@@ -23,54 +22,45 @@ var getNumberFromInput = function (inputParam) {
     return result;
 }
 
-var onOperationBtnClick = function(event) {
-    var pressedOperationButton = event.target;
-    var operationValue = pressedOperationButton.Value;
+var onOperationBtnClick = function (e) {
+    var pressedOperationButton = e.target;
+    var operationValue = pressedOperationButton.value;
     var numbersValuesObj = getNumbers();
-     
 
-     if (operationValue == '+') {
+
+    if (operationValue == '+') {
         var resultValue = numbersValuesObj.num1 + numbersValuesObj.num2
-    }   else if (operationValue == '+') {
+    } else if (operationValue == '-') {
         var resultValue = numbersValuesObj.num1 - numbersValuesObj.num2
-        }
-
-    window.alert(resultValue); 
+    } else if (operationValue == '*') {
+        var resultValue = numbersValuesObj.num1 * numbersValuesObj.num2
+    } else if (operationValue == '/') {
+        var resultValue = numbersValuesObj.num1 / numbersValuesObj.num2
     }
+
+
+    window.alert(resultValue);
+}
 
 //plus button logic
 var onClickPlusButton = function () {
-  /*   var numbersValuesObj = getNumbers();
 
-    var resultValue = numbersValuesObj.num1 + numbersValuesObj.num2;
-    window.alert(resultValue); */
 }
 numberPlusButton.addEventListener('click', onOperationBtnClick);
 
 //minus button logic
 var onClickMinusButton = function () {
-   /*  var numbersValuesObj = getNumbers();
 
-    var resultValue = numbersValuesObj.num1 - numbersValuesObj.num2;
-    window.alert(resultValue); */
 }
 numberMinusButton.addEventListener('click', onOperationBtnClick);
 
 //multiply button logic
 var onClickMultiplyButton = function () {
-/*     var numbersValuesObj = getNumbers();
-
-    var resultValue = numbersValuesObj.num1 * numbersValuesObj.num2;
-    window.alert(resultValue); */
 }
 numberMultiplyButton.addEventListener('click', onOperationBtnClick);
 
 //divide button logic
 var onClickDivideButton = function () {
-/*     var numbersValuesObj = getNumbers();
-
-    var resultValue = numbersValuesObj.num1 / numbersValuesObj.num2;
-    window.alert(resultValue); */
 }
 numberDivideButton.addEventListener('click', onOperationBtnClick);
 
